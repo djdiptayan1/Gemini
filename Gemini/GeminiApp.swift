1106bde5e95a9af9afd29aa5f1dfd3e2b1493c2b
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-19 21:05:24 +0530

    Initial Commit

---
+++ Gemini/GeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiApp.swift
+//  Gemini
+//
+//  Created by Diptayan Jash on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
