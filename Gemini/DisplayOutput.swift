f732dd5aec915b4443982a61a42ecf3c612d4a65
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-20 13:07:54 +0530

    Refactor DisplayOutput view to use a ScrollView and Message struct

--- Gemini/DisplayOutput.swift
+++ Gemini/DisplayOutput.swift
@@ -8,32 +8,70 @@
 import Foundation
 import SwiftUI
 
+// struct DisplayOutput: View {
+//    @Binding var SentText: String
+//    @Binding var outputText: String
+//
+//    var body: some View {
+//        VStack(alignment: .leading, spacing: 10) {
+//            HStack {
+//                Text(SentText)
+//                    .padding(8)
+//                    .foregroundColor(.white)
+//                    .background(SentText.isEmpty ? Color.clear : Color(.blue))
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//            }
+//
+//            HStack {
+//                Spacer()
+//                Text(outputText)
+//                    .padding(8)
+//                    .foregroundColor(.white)
+//                    .background(outputText.isEmpty ? Color.clear : Color(.green))
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//                    .frame(maxWidth: .infinity, alignment: .trailing)
+//            }
+//        }
+//        .padding()
+//    }
+// }
+
+struct Message: Identifiable {
+    let id = UUID()
+    let text: String
+    let isSent: Bool
+}
+
 struct DisplayOutput: View {
-    
-    @Binding var SentText: String
-    @Binding var outputText: String
-    
+    @Binding var messages: [Message]
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
-            HStack {
-                Text(SentText)
-                    .padding(8)
-                    .foregroundColor(.white)
-                    .background(SentText.isEmpty ? Color.clear : Color(.blue))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .frame(maxWidth: .infinity, alignment: .leading)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                ForEach(messages) { message in
+                    HStack {
+                        if message.isSent {
+                            Spacer()
+                            Text(message.text)
+                                .padding(8)
+                                .foregroundColor(.white)
+                                .background(message.text.isEmpty ? Color.clear : Color(.blue))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        } else {
+                            Text(message.text)
+                                .padding(8)
+                                .foregroundColor(.white)
+                                .background(message.text.isEmpty ? Color.clear : Color(.green))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                }
             }
-            
-            HStack {
-                Spacer()
-                Text(outputText)
-                    .padding(8)
-                    .foregroundColor(.white)
-                    .background(outputText.isEmpty ? Color.clear : Color(.green))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-            }
+            .padding()
         }
-        .padding()
     }
 }
+
