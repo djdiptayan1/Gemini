925335a16111351875bb09e62e61841db4185841
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-01-21 15:32:54 +0530

    removed redunt code.

--- Gemini/DisplayOutput.swift
+++ Gemini/DisplayOutput.swift
@@ -8,35 +8,6 @@
 import Foundation
 import SwiftUI
 
-// struct DisplayOutput: View {
-//    @Binding var SentText: String
-//    @Binding var outputText: String
-//
-//    var body: some View {
-//        VStack(alignment: .leading, spacing: 10) {
-//            HStack {
-//                Text(SentText)
-//                    .padding(8)
-//                    .foregroundColor(.white)
-//                    .background(SentText.isEmpty ? Color.clear : Color(.blue))
-//                    .clipShape(RoundedRectangle(cornerRadius: 10))
-//                    .frame(maxWidth: .infinity, alignment: .leading)
-//            }
-//
-//            HStack {
-//                Spacer()
-//                Text(outputText)
-//                    .padding(8)
-//                    .foregroundColor(.white)
-//                    .background(outputText.isEmpty ? Color.clear : Color(.green))
-//                    .clipShape(RoundedRectangle(cornerRadius: 10))
-//                    .frame(maxWidth: .infinity, alignment: .trailing)
-//            }
-//        }
-//        .padding()
-//    }
-// }
-
 struct Message: Identifiable, Equatable {
     let id = UUID()
     let text: String
